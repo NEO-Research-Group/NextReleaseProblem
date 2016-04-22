9a977cdb6c5762f18fc4296ed782e7c87f489b68
francis <chicano@lcc.uma.es>
2016-04-22 17:28:40 +0200

    Small edit (spaces)

--- src/main/java/neo/requirements/sat/cplex/CplexSolver.java
+++ src/main/java/neo/requirements/sat/cplex/CplexSolver.java
@@ -1,15 +1,8 @@
 package neo.requirements.sat.cplex;
 
 import ilog.concert.IloException;
+import ilog.concert.IloNumVar;
 import ilog.cplex.IloCplex;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.PrintWriter;
-
-import neo.requirements.sat.minisatp.MinisatReader;
-import neo.requirements.sat.minisatp.MinisatpSolver.SearchDirection;
 
 public class CplexSolver {
 	
@@ -29,7 +22,6 @@ public class CplexSolver {
 	public Result solveIlpInstance(Modelo modelo) {	
 		try {
 			modelo.cplex.setOut(null);
-			
 			
 			modelo.cplex.setParam(IloCplex.DoubleParam.EpInt, 1E-9);
 			modelo.cplex.setParam(IloCplex.DoubleParam.EpGap, 1E-9);
